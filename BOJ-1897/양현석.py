8ae9675bca2270b5a8ce9fa0767142ad58e0782c
Piousangel <yyh7654@gmail.com>
2022-04-29 23:38:11 +0900

    4/29

--- BOJ-1897/양현석.py
+++ BOJ-1897/양현석.py
@@ -1,42 +1,75 @@
 import sys
+
 sys.stdin = open('sample.txt')
 input = sys.stdin.readline
 
 n, str1 = map(str, input().split())
 str_list = []
 
-
 for i in range(int(n)):
     str_list.append(input().rstrip())       #사전에 등재된 단어만 사용가능
 
-dictionary = list(str1.rstrip())
+str_list.sort(key = len)
+# print(str_list)
 
-for strs in str_list :
-    for i in range(1, len(strs)-1):
-        if strs[i] not in dictionary :
-            dictionary.append(strs[i])
-            
-# print(dictionary)
-temp_len = 0
-answer = ""
+dic = []
+dic.append(str1)      #기본 추가
 
-for strs in str_list:                #무조건 3글자
-    # print(strs[-2:])
-    # print(strs[:2])
-    if str1[0] not in strs[:2] : #s
-        continue
-    if str1[2] not in strs[-2:] : #d
-        continue
+for strs in str_list:
     
-    # flag = True
-    for i in range(0, len(strs)):  #mid
-        if strs[i] not in dictionary:
-            # flag = False
+    if strs in dic :
+        continue
+
+    for i in range(len(strs)):
+        print(strs[:i] + strs[i+1:])
+        temp = strs[:i+1] + strs[i+2:]
+        if temp in dic :
+            dic.append(strs)
             break
-    else :
-        if str1[1] in strs[1:-1]:
-            if temp_len < len(strs) :
-                temp_len = len(strs)
-                answer = strs
+
+print (max(dic, key=len))
+
+
+
+# import sys
+# sys.stdin = open('sample.txt')
+# input = sys.stdin.readline
+
+# n, str1 = map(str, input().split())
+# str_list = []
+
+
+# for i in range(int(n)):
+#     str_list.append(input().rstrip())       #사전에 등재된 단어만 사용가능
+
+# dictionary = list(str1.rstrip())
+
+# for strs in str_list :
+#     for i in range(1, len(strs)-1):
+#         if strs[i] not in dictionary :
+#             dictionary.append(strs[i])
+            
+# # print(dictionary)
+# temp_len = 0
+# answer = ""
+
+# for strs in str_list:                #무조건 3글자
+#     # print(strs[-2:])
+#     # print(strs[:2])
+#     if str1[0] not in strs[:2] : #s
+#         continue
+#     if str1[2] not in strs[-2:] : #d
+#         continue
+    
+#     # flag = True
+#     for i in range(0, len(strs)):  #mid
+#         if strs[i] not in dictionary:
+#             # flag = False
+#             break
+#     else :
+#         if str1[1] in strs[1:-1]:
+#             if temp_len < len(strs) :
+#                 temp_len = len(strs)
+#                 answer = strs
            
-print(answer)
+# print(answer)
