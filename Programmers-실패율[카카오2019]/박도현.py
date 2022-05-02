c4c11c374ba9d1624246d5346fb01121f6ff2ecf
dd0114 <ehgus0114@gmail.com>
2022-05-02 23:57:38 +0900

    도현

---
+++ Programmers-실패율[카카오2019]/박도현.py
@@ -0,0 +1,31 @@
+def solution(N, stages):
+    answer = []
+    clear = [0]*len(stages+1)
+    dodal = [0]*len(stages+1)
+    ans = []
+    ans2 = []
+    maxnum = 2
+    for i in stages:
+        for j in range(1,len(i)):
+            dodal[j] +=1
+            clear[j] +=1
+        clear[i] -= 1
+    
+    for i in range(1,N+1):
+        a = clear[i]/dodal[i]
+        ans.append(a)
+        ans2.append(a)
+    
+    ans2.sort()
+    
+    for i in range(1,len(ans)):
+        a = ans2.index(ans[i])
+        answer.append(a)
+        ans2[a] = 2
+    
+    return answer
+
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+print(solution(4, [4, 4, 4, 4, 4]))
+print(solution(4, [1, 2, 2, 2, 0]))
+print(solution(3, [1, 1, 1]))
