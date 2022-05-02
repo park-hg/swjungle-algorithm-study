ac4ff8fc69882df42e31a5cd420191c9509e22be
Piousangel <yyh7654@gmail.com>
2022-05-02 09:45:20 +0900

    Q

--- BOJ-1561/양현석.py
+++ BOJ-1561/양현석.py
@@ -21,37 +21,41 @@ else:
     left_value = 0
     right_value = wait_list[-1] * N
     answer = 0
-    
+    total_people = N #N-M
     # print(wait_list)
     
     while left_value <= right_value :
 
         mid_vlaue = (left_value + right_value) // 2
         total = 0
+        temp_value = wait_list[-1]
         temp_idx = 0
         idx_list = []
+        # onemin_list = []
+
         # print(mid_vlaue, end=" ")
         for i in range(M):
             
             # print(mid_vlaue % wait_list[i], end=" ")
             idx_list.append(mid_vlaue % wait_list[i])
-      
+            # onemin_list.append((mid_vlaue-1) % wait_list[i])
+            
             total += mid_vlaue // wait_list[i]   # 1 2 3 4 5
        
-        # print("mid", mid_vlaue)
+        print("mid", mid_vlaue)
         # print("total", total)
-        # print(idx_list)
-        if total < N :
+        print(idx_list)
+
+        if total < total_people :
             left_value = mid_vlaue + 1
         else:
             right_value = mid_vlaue - 1
-            answer = mid_vlaue
+            
             for j in range(len(idx_list)):          #순서대로 가면서 
-                if idx_list[j] > temp_idx:
-                    temp_idx = idx_list[j]
-                    answer = copy_list.index(wait_list[j]) +1
+                if idx_list[j] <= temp_value:
+                    temp_value = idx_list[j]
+                    temp_idx = j
+                    answer = copy_list.index(wait_list[temp_idx])+1 
 
     print(answer)
-            # print(idx_list)
-    # print(answer)
-    # print(real_idx)
+ 
