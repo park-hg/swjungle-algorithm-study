786599413b3c702f0669e36407f401a83246ce71
dd0114 <ehgus0114@gmail.com>
2022-05-03 00:28:36 +0900

    re

--- Programmers-실패율[카카오2019]/박도현.py
+++ Programmers-실패율[카카오2019]/박도현.py
@@ -1,30 +1,34 @@
 def solution(N, stages):
     answer = []
-    clear = [0]*len(stages+1)
-    dodal = [0]*len(stages+1)
+    clear = [0]*(N+2)
+    dodal = [0]*(N+2)
     ans = []
     ans2 = []
     for i in stages:
-        for j in range(1,len(i)):
+        for j in range(1,i+1):
             dodal[j] +=1
             clear[j] +=1
         clear[i] -= 1
     
     for i in range(1,N+1):
-        a = clear[i]/dodal[i]
-        ans.append(a)
-        ans2.append(a)
+        if dodal[i] != 0:
+            a = clear[i]/dodal[i]
+            ans.append(a)
+            ans2.append(a)
+        else :
+            ans.append(0)
+            ans2.append(0)
     
     ans2.sort()
     
-    for i in range(1,len(ans)):
-        a = ans2.index(ans[i])
-        answer.append(a)
-        ans2[a] = 2
+    for i in range(len(ans)):
+        a = ans.index(ans2[i])
+        answer.append(a+1)
+        ans[a] = 2
     
     return answer
 
-print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
+# print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
 print(solution(4, [4, 4, 4, 4, 4]))
-print(solution(4, [1, 2, 2, 2, 0]))
-print(solution(3, [1, 1, 1]))
+# print(solution(4, [1, 2, 2, 2, 0]))
+# print(solution(3, [1, 1, 1]))
