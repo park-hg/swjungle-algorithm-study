e04c9e7c4c7589e9843aee7f1c7242be61d0f030
Seungwoo Son <jsmith032211@gmail.com>
2022-05-05 00:03:03 +0900

    Update Son0-0.py

--- Programmers-양과 늑대/Son0-0.py
+++ Programmers-양과 늑대/Son0-0.py
@@ -1,35 +1,25 @@
-# On Going
-
 max_value = 0
 
 def dfs(s, graph, info, sh, wlf, odr_list):
-  global max_value
-  
-  if sh == wlf:
-    return sh
+    global max_value
 
-  for idx in odr_list:
-    temp = [num for num in odr_list if num != idx]
-    if info[idx] == 0:
-      max_value = max(max_value, dfs(idx, graph, info, sh + 1, wlf, temp + graph[idx]))
-    else:
-      max_value = max(max_value, dfs(idx, graph, info, sh, wlf + 1, temp + graph[idx]))
-      
-  return sh
+    if sh == wlf:
+        return
+
+    max_value = max(max_value, sh)
+    for idx in odr_list:
+        temp = [num for num in odr_list if num != idx]
+        if info[idx] == 0:
+            dfs(idx, graph, info, sh + 1, wlf, temp + graph[idx])
+        else:
+            dfs(idx, graph, info, sh, wlf + 1, temp + graph[idx])
 
 def solution(info, edges):
-    size = len(info)
-    graph = [[] for _ in range(size)]
-    odr_list = []
-    
+    graph = [[] for _ in range(len(info))]
+
     for s, d in edges:
-      graph[s].append(d)
+        graph[s].append(d)
 
     dfs(0, graph, info, 1, 0, graph[0])
     
-    print(max_value)
-  
-info = [0,0,1,1,1,0,1,0,1,0,1,1]
-edges = [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]
-
-solution(info, edges)
+    return max_value
