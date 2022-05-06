d3c265c5da79dd3c093aa1eb79317fdf2add0b2e
이혜진 <annie_1229@naver.com>
2022-05-06 12:59:04 +0900

    [이혜진] 8번째 문제

--- LEET-Longest Substring Without Repeating Characters/annie1229.py
+++ LEET-Longest Substring Without Repeating Characters/annie1229.py
@@ -1,1 +1,21 @@
-# 0506 https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# 8번째 문제 0506 https://leetcode.com/problems/longest-substring-without-repeating-characters/
+from collections import deque
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        substring = deque()
+        max_length = 0
+        for ss in s:
+            if ss in substring:
+                pop = substring.popleft()
+                while substring and pop != ss:
+                    pop = substring.popleft()
+            substring.append(ss)
+            max_length = max(max_length, len(substring))
+        return max_length
+
+sol = Solution()
+sol.lengthOfLongestSubstring("abcabcbb")
+sol.lengthOfLongestSubstring("bbbbb")
+sol.lengthOfLongestSubstring("pwwkew")
+sol.lengthOfLongestSubstring("aab")
