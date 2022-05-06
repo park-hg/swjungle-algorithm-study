c3eb90f6908daa34e2652455fc59f0d63b474727
Piousangel <yyh7654@gmail.com>
2022-05-06 10:58:25 +0900

    5/6

---
+++ BOJ-14627/Piousangel.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+pa_n, dak_n = map(int, input().split())
+pa_list = []
+for i in range(pa_n) :
+    pa_list.append(int(input()))
+
+pa_list.sort()
+left = 1
+right = pa_list[-1]
+mid = 0
+temp = 0
+answer = 0
+# print(pa_list)
+
+while left <= right :
+    
+    mid = (right + left) // 2
+      
+    total = 0
+    for pa in pa_list :
+        total += pa // mid
+    
+    # print("mid", mid)
+    # print("total", total)
+    if total >= dak_n :
+        temp = mid
+        left = mid +1
+    else:
+        right = mid -1
+
+answer = sum(pa_list) - (temp * dak_n)
+
+print(answer)
+
+
