dd310138fef3563ac2960d33fcb7ed2ad2d4b3d5
Seungwoo Son <jsmith032211@gmail.com>
2022-05-07 11:40:54 +0900

    Update Son0-0.py

--- Programmers-다단계 칫솔 판매/Son0-0.py
+++ Programmers-다단계 칫솔 판매/Son0-0.py
@@ -1,1 +1,50 @@
-import sys
+def calculate_pay(p):
+    if p < 10:
+        return 0
+
+    return int(p * 0.1)
+
+def solution(enroll, referral, seller, amount):
+    
+    conn = {}
+
+    for idx, name in enumerate(referral):
+        if enroll[idx] not in conn:
+            conn[enroll[idx]] = name
+        else:
+            conn[enroll[idx]].append(name)
+
+    result = {}
+
+    for idx, p in enumerate(amount):
+        target = seller[idx]
+        pay = (p * 100)
+        fee = calculate_pay(pay)
+
+        while True:
+            if pay == 0:
+                break
+                
+            if target not in result:
+                result[target] = pay - fee
+            else:
+                result[target] += pay - fee
+            target = conn[target]
+            pay = int(pay * 0.1)
+            fee = calculate_pay(pay)
+
+            if target == "-":
+                if target not in result:
+                    result[target] = pay
+                else:
+                    result[target] += pay
+                break
+
+    answer = []
+    for ename in enroll:
+        if ename in result:
+            answer.append(result[ename])
+        else:
+            answer.append(0)
+
+    return answer
