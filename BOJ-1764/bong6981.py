39ca24485e202093323a181803893761cb6be1e3
Bongf <73228803+bong6981@users.noreply.github.com>
2022-05-11 23:38:13 +0900

    Update bong6981.py

--- BOJ-1764/bong6981.py
+++ BOJ-1764/bong6981.py
@@ -1,1 +1,15 @@
-//hi 
+N, M = map(int, input().split())
+
+didnt_listen = set()
+didnt_see = set()
+
+for _ in range(N):
+    didnt_listen.add(input())
+
+for _ in range(M):
+    didnt_see.add(input())
+
+ans = sorted(list(didnt_listen & didnt_see))
+print(len(ans))
+for name in ans:
+    print(name)
