a67331d8e380c08e7ddd56409e74c085bae92c6d
이혜진 <annie_1229@naver.com>
2022-05-17 23:48:16 +0900

    [이혜진] 19번째 문제

--- BOJ-16987/annie1229.py
+++ BOJ-16987/annie1229.py
@@ -1,4 +1,4 @@
-# 19. 계란으로 계란치기(BOJ-16987) 아직 푸는 중입니다..ㅠ
+# 19. 계란으로 계란치기(BOJ-16987)
 import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
@@ -16,20 +16,20 @@ if N == 1:
 max_count = 0
 check = [False for _ in range(N)]
 
-def dfs(eggs_list, cur_idx, is_egg_break, break_egg_count):
-    # print(eggs_list[cur_idx], 'dfs ', eggs_list, is_egg_break, break_egg_count)
+def dfs(eggs_list, cur_idx, is_egg_break):
     global max_count
+    break_egg_count = is_egg_break.count(True)
     max_count = max(max_count, break_egg_count)
-    if is_egg_break.count(True) == N:
+
+    if break_egg_count == N:
         print(N)
         exit(0)
 
     if cur_idx == N:
-        # print('마지막 계란입니다. ', eggs_list, is_egg_break, break_egg_count - 1)
-        max_count = max(max_count, break_egg_count)
         return 
 
     if is_egg_break[cur_idx]:
+        dfs(eggs_list, cur_idx + 1, is_egg_break)
         return
 
     cur_egg = eggs_list[cur_idx]
@@ -44,20 +44,16 @@ def dfs(eggs_list, cur_idx, is_egg_break, break_egg_count):
             eggs_list[cur_idx] = (durability_egg1, cur_egg[1])
             eggs_list[idx] = (durability_egg2, next_egg[1])
             if durability_egg1 <= 0:
-                break_egg_count += 1
                 is_egg_break[cur_idx] = True
             if durability_egg2 <= 0:
-                break_egg_count += 1
                 is_egg_break[idx] = True
-            dfs(eggs_list, cur_idx + 1, is_egg_break, break_egg_count)
+            dfs(eggs_list, cur_idx + 1, is_egg_break)
             eggs_list[cur_idx] = cur_egg
             eggs_list[idx] = next_egg
             if durability_egg1 <= 0:
-                break_egg_count -= 1
                 is_egg_break[cur_idx] = False
             if durability_egg2 <= 0:
-                break_egg_count -= 1
                 is_egg_break[idx] = False
 
-dfs(eggs, 0, check, 0)
+dfs(eggs, 0, check)
 print(max_count)
