e2936d370bae003c6f5425f9307830cd7ccf2386
Seungwoo Son <jsmith032211@gmail.com>
2022-05-22 01:46:30 +0900

    Update Son0-0.py

--- BOJ-3885_회색영역/Son0-0.py
+++ BOJ-3885_회색영역/Son0-0.py
@@ -1,4 +1,4 @@
-# 문제 푸는중입니다 
+# : (
 import sys
 
 input = sys.stdin.readline
@@ -7,5 +7,26 @@ def solution():
   while True:
     n, w = map(int, input().split())
     if n == 0 and w == 0 : return
+    numlist = []
+    for _ in range(n):
+      numlist.append(int(input()))
+      
+    # 구간 수
+    num_of_interval = (max(numlist) // w) + 1
+    interval = [0 for _ in range(num_of_interval)]
+    
+    # max_value == 가장 높은 막대
+    max_value = 0
+    for num in numlist:
+      idx = num // w
+      interval[idx] += 1
+      max_value = max(max_value, interval[idx])
+    
+    denominator = num_of_interval - 1
+    ink = 0.01
+    for idx, size in enumerate(interval):
+      ink += (size / max_value) * ((denominator - idx) / denominator)
+      
+    print(ink)
     
 solution()
