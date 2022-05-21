794748ec8dbf7c1350366ae2bafa6678e6d57cfa
이혜진 <annie_1229@naver.com>
2022-05-22 01:28:40 +0900

    [이혜진] 23번째 문제

--- BOJ-3885_회색영역/annie1229.py
+++ BOJ-3885_회색영역/annie1229.py
@@ -1,4 +1,4 @@
-# 23. 회색 영역(BOJ-3885) 푸는 중입니다ㅠㅠ
+# 23. 회색 영역(BOJ-3885)
 import sys
 sys.stdin = open('input.txt')
 input = sys.stdin.readline
@@ -7,22 +7,22 @@ while(1):
     histo_n, width = map(int, input().split())
     if histo_n == 0 and width == 0:
         break
-    histo_height = []
-    max_height = 0
-    for _ in range(histo_n):
-        h = int(input())
-        histo_height.append(h)
-        max_height = max(max_height, h)
 
+    histo_height = [0 for _ in range(11)]
+    max_data = 0
+    for _ in range(histo_n):
+        data = int(input())
+        histo_height[data//width] += 1
+        max_data = max(max_data, data)
+        
+    max_idx = max_data // width
+    max_height = max(histo_height)
     max_area = width * max_height
-    print('max h >> ', max_height, max_area)
     area_ink = 1 / max_area
     total_ink = 0
-    color = 1
-    for hh in histo_height:
+
+    for idx, hh in enumerate(histo_height):
         area = hh * width
-        total_ink += color * area_ink * area
-        print('color ', color, area)
-        color -= (color / histo_n)
+        total_ink += ((max_idx - idx) / max_idx) * area_ink * area
     total_ink += 0.01
-    print(area_ink, total_ink)
+    print(total_ink)
