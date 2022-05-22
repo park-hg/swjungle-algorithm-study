c4ae90484ecd2d60fa2f4f626fe6e534fc422d6d
이혜진 <annie_1229@naver.com>
2022-05-23 00:33:23 +0900

    [이혜진] 24번째 문제

--- BOJ-17225_세훈이의 선물가게/annie1229.py
+++ BOJ-17225_세훈이의 선물가게/annie1229.py
@@ -10,23 +10,30 @@ sangmin_packing_count = 0
 sangmin_packing_list = []
 jisoo_packing_count = 0
 jisoo_packing_list = []
+sangmin_last_packing_time = 0
+jisoo_last_packing_time = 0
 
 for n in range(total_guest_count):
     order_time, packing_color, gifts = input().rstrip().split()
     if packing_color == 'B': 
         sangmin_packing_count += int(gifts)
+        sangmin_last_packing_time = max(int(order_time), sangmin_last_packing_time)
         for c in range(int(gifts)):
-            packing = (int(order_time) + (c * sangmin_packing_time), packing_color)
+            packing = (sangmin_last_packing_time + (c * sangmin_packing_time), packing_color)
             heappush(order_list, packing)
+        sangmin_last_packing_time += (int(gifts) * sangmin_packing_time)
     else: 
         jisoo_packing_count += int(gifts)
+        jisoo_last_packing_time = max(int(order_time), jisoo_last_packing_time)
         for c in range(int(gifts)):
-            packing = (int(order_time) + (c * sangmin_packing_time), packing_color)
+            packing = (jisoo_last_packing_time + (c * jisoo_packing_time), packing_color)
             heappush(order_list, packing)
+        jisoo_last_packing_time += (int(gifts) * jisoo_packing_time)
 
 gift_idx = 1
 while order_list:
     packing_time, packing_color = heappop(order_list)
+    print(packing_time, packing_color)
     if packing_color == 'B':
         sangmin_packing_list.append(str(gift_idx))
     else:
