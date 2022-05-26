839c5b090cebf1d0f11bfae66f0f53181fcdf702
Piousangel <yyh7654@gmail.com>
2022-05-27 02:07:46 +0900

    부분합 다시 풀었습니다

--- BOJ-1806/piousangel.py
+++ BOJ-1806/piousangel.py
@@ -1,6 +1,5 @@
 import sys
 from collections import deque
-sys.stdin = open('sample.txt')
 input = sys.stdin.readline
 
 n, s = map(int, input().split())
@@ -17,11 +16,12 @@ def search(n_list, s) :
 
     while q :
         if idx == len(n_list)-1:
-            while len(q) >= 1 :  #마지막 인덱스까지 왔으면 앞에거 빼주면서 s보다 큰거 있는지 chk 
-                temp = q.popleft()
-                total -= temp
+            while q:  #마지막 인덱스까지 왔으면 앞에거 빼주면서 s보다 큰거 있는지 chk 
                 if total >= s :
                     answer = min(answer, len(q))
+                temp = q.popleft()
+                total -= temp
+                
             break
 
         if total < s :
