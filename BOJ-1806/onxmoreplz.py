460ed009435aa4350ddf14b9a066273b94e8e7e9
Doyoung Ham <70103130+onxmoreplz@users.noreply.github.com>
2022-05-27 21:57:33 +0900

    fix: two pointer

--- BOJ-1806/onxmoreplz.py
+++ BOJ-1806/onxmoreplz.py
@@ -1,28 +1,27 @@
-## 시간 초과
-
+## 재 도 전
 
 import sys
 
 N, S = map(int, sys.stdin.readline().split())
 nums = list(map(int, sys.stdin.readline().split()))
-dp = [0] * N
 
-flag = False
-answer = 0
+
 if sum(nums) < S:
   print(0)
 else:
-  while not flag:
-    
-    for i in range(N - answer):
-      dp[i] += nums[i+answer]
-      if dp[i] >= S:
-        flag = True
-        break
-    # print(dp)
-        
-      
-    answer += 1
-  
-  if flag:
-    print(answer)
+  lp, rp = 0, 0
+  sums = 0
+  answer = sys.maxsize
+  while True:
+    if sums >= S:
+      answer = min(answer, rp - lp)
+      sums -= nums[lp]
+      lp += 1
+    elif rp == N:
+      break
+    else:
+      sums += nums[rp]
+      rp += 1
+
+
+  print(answer)
