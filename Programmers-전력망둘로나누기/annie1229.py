4600373a5edce3f0a596a84fa520b3655f3ab72d
이혜진 <annie_1229@naver.com>
2022-05-29 23:01:40 +0900

    이혜진

--- Programmers-전력망둘로나누기/annie1229.py
+++ Programmers-전력망둘로나누기/annie1229.py
@@ -12,7 +12,6 @@ def dfs(n, graph, visited, check):
 
 def solution(n, wires):
     # 1개를 끊어서 최대한 비슷한 개수로 나누기
-    answer = -1
     wire_graph = [[] for _ in range(n + 1)]
     for idx, wire in enumerate(wires):
       a, b = wire
@@ -22,25 +21,19 @@ def solution(n, wires):
       
     check = [False for _ in range(len(wires))]
     min_diff = n
-    min_cnt = 0
+    
     for idx, wire in enumerate(wires):
       visited = [False for _ in range(n + 1)]
-      check = [False for _ in range(len(wires))]
       check[idx] = True
       dfs(1, wire_graph, visited, check)
-      print(idx, wire, visited.count(True))
+      check[idx] = False
       true_cnt = visited.count(True)
-      false_cnt = visited.count(False) - 1
+      false_cnt = n - true_cnt
       diff = abs(true_cnt - false_cnt)
-      print('diff', diff)
       if diff < min_diff:
         min_diff = diff
-        min_cnt = true_cnt
-    print('min diff', min_diff, min_cnt)
-    a = n - min_cnt;
-    answer = abs(min_cnt - a)
-    print('ans',answer)
-    return answer
+
+    return min_diff
 
 solution(9, [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]])
 solution(4, [[1,2],[2,3],[3,4]])
