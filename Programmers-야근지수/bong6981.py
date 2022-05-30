d8592033805bf404e8cc4e4f0e80f30feb969937
Bongf <73228803+bong6981@users.noreply.github.com>
2022-05-30 22:09:10 +0900

    Create bong6981.py

---
+++ Programmers-야근지수/bong6981.py
@@ -0,0 +1,26 @@
+def solution(n, works):
+    
+    ## n만큼 일할 수 있는데, works에서 n만큼 빼서, 그 제곱을 더한 값이 최소가 되도록 한다. 
+    
+    left = n
+    while True:
+        works.sort(reverse=True)
+        if left == 0:
+            break
+        max_v = works[0]
+        if max_v == 0:
+            break
+        for i in range(len(works)):
+            if left == 0:
+                break
+            if works[i] == max_v:
+                works[i] -= 1
+                left -= 1
+            else:
+                break
+        
+    answer = 0
+    for i in works:
+        answer += i ** 2
+    
+    return answer
