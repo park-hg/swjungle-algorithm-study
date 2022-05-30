fddddfbaf3d1714bfe93e60883041edfd6a4711a
이혜진 <annie_1229@naver.com>
2022-05-30 22:47:38 +0900

    다시 풀기

--- Programmers-야근지수/annie1229.py
+++ Programmers-야근지수/annie1229.py
@@ -6,32 +6,15 @@ def calc(arr):
   return total
 
 def solution(n, works):
-    answer = 0
-    total_time = sum(works)
-    after_time = total_time - n
-
-    if after_time <= 0:
+    if sum(works) <= n:
       return 0
-
-    div_time = after_time // len(works) # 남은 시간을 균등분배했을 때 시간
-    remain = after_time % len(works) # 더 일해야하는 시간(나머지)
-    new_arr = []
-
-    for work in works:
-      if div_time <= work:
-        new_arr.append(div_time)
-      else:
-        new_arr.append(work)
-        remain += (div_time - work)
-
-    for i in range(1, remain + 1):
-      new_arr[i % len(works)] += 1
-    
-    answer = calc(new_arr)
-    print('div', div_time, 'remain', remain, 'newarr', new_arr, 'ans >> ',answer)
-    return answer
-
-solution(4, [4, 3, 3])
-solution(1, [2, 1, 2])
-solution(3, [1,1])
-print(calc([2,2,2]))
+    while n:
+      works.sort(reverse=True)
+      max_num = max(works)
+      for i in range(len(works)):
+        if works[i] == 0: continue
+        if n == 0: break
+        if max_num <= works[i]:
+          works[i] -= 1
+          n -= 1
+    return calc(works)
