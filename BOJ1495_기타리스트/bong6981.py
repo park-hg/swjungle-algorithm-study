a0da6c65eae15e0e9037f987ab776f5610eb0b78
Bongf <73228803+bong6981@users.noreply.github.com>
2022-06-06 22:17:16 +0900

    Update bong6981.py

--- BOJ1495_기타리스트/bong6981.py
+++ BOJ1495_기타리스트/bong6981.py
@@ -1,1 +1,27 @@
-## pintos
+import sys
+input = sys.stdin.readline
+
+N, S, M = map(int, input().split())
+nums = list(map(int, input().split()))
+
+
+can = [S]
+ans = 0
+for num in nums:
+    print(num, can)
+    new_can = set()
+    for c in can:
+        print(c, c-num, c+num)
+        if 0<= (c + num) <= M:
+            new_can.add(c+num)
+        if 0 <= (c - num) <= M:
+            new_can.add(c-num)
+    can = new_can
+    if len(new_can) == 0:
+        ans = -1
+        break
+if ans == -1:
+    print(ans)
+else:
+    print(max(can))
+
