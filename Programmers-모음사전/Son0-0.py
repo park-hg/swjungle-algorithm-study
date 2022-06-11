e9d1ee6ae77852d48689030c1d03f52df11ff192
Seungwoo Son <jsmith032211@gmail.com>
2022-06-11 22:29:30 +0900

    Update Son0-0.py

--- Programmers-모음사전/Son0-0.py
+++ Programmers-모음사전/Son0-0.py
@@ -5,13 +5,10 @@ def solution(word):
     llist = []
 
     def dfs(visited=""):
-        global answer
-
         if 5 <= len(visited):
             return
 
         for a in alpha:
-            answer += 1
             llist.append(visited + a)
             dfs(visited + a)
 
