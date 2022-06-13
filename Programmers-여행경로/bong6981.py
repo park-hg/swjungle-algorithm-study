8816f23c1f1a838a242888f0b35b2a273950496a
Bongf <73228803+bong6981@users.noreply.github.com>
2022-06-13 22:25:40 +0900

    Update bong6981.py

--- Programmers-여행경로/bong6981.py
+++ Programmers-여행경로/bong6981.py
@@ -1,4 +1,6 @@
 from collections import defaultdict, deque
+from pickle import TRUE
+
 
 def solution(tickets):
     tickets.sort()
@@ -7,27 +9,30 @@ def solution(tickets):
     for i, t in enumerate(tickets):
         s = t[0]
         e = t[1]
-        info[s].append(e)
+        info[s].append(i)
     
-    # def dfs(arr):
-    #     if len(arr) == len(tickets) + 1:
-    #         return arr
+    visited = [0] * len(tickets)
+    def dfs(arr):
+        if len(arr) == len(tickets) + 1:
+            return arr
         
-    #     start = arr[-1]
-    #     des = info[start].popleft()
-    #     return dfs(arr+[des])
+        start = arr[-1]
+        for i in info[start]:
+            if not visited[i]:
+                des = tickets[i][1]
+                visited[i] = TRUE
+                ret = dfs(arr+[des])
+                visited[i] = False
+                if ret != []:
+                    return ret
+        
+        return []
     
-    def other(arr, start):
-
-        while info[start]:
-            des = info[start].popleft()
-            other(arr, des)
-        
-        arr.append(start)
-        
-    arr = []
-    other(arr, "ICN")
-    return arr[::-1]
+    dfs(["ICN"])
+    return dfs(["ICN"])
+    # arr = []
+    # other(arr, "ICN")
+    # return arr[::-1]
     
     
 
